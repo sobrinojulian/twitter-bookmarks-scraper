9c59acf29b035b9508035b49bce21904392bc671
Sobrino Julian <sobrinojulian@protonmail.com>
2020-03-08 16:58:49 -0300

    Meta refactor por ahora funca

--- getTweets.js
+++ getTweets.js
@@ -1,5 +1,4 @@
-const SELECTORS = require(`./selectors`);
-const { innerText, src, dateTime } = require(`./helpers`);
+const getTweet = require(`./getTweet`)
 
 const getTweets = async page => {
   const url = `https://mobile.twitter.com/i/bookmarks`;
@@ -8,30 +7,8 @@ const getTweets = async page => {
 
   const tweets = await page.$$(`article`);
   for (const tweet of tweets) {
-    const avatar = await tweet.$(SELECTORS.avatar);
-    const id = await tweet.$(SELECTORS.id);
-    const name = await tweet.$(SELECTORS.name);
-
-    const date = await tweet.$(SELECTORS.date);
-    const replyTo = await tweet.$(SELECTORS.replyTo);
-    const text = await tweet.$(SELECTORS.text);
-
-    const replies = await tweet.$(SELECTORS.replies);
-    const retweets = await tweet.$(SELECTORS.retweets);
-    const likes = await tweet.$(SELECTORS.likes);
-
-    console.log(await src(avatar));
-    console.log(await innerText(id));
-    console.log(await innerText(name));
-
-    console.log(await dateTime(date))
-    console.log(await innerText(replyTo));
-    console.log(await innerText(text));
-
-    console.log(await innerText(replies));
-    console.log(await innerText(retweets));
-    console.log(await innerText(likes));
-    console.log();
+    await getTweet(tweet)
   }
 };
+
 module.exports = getTweets;
